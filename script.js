35ac6b87baf32f345ab9d61341a595d8ba60adea
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-11-23 08:21:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+// let inputElement = document.getElementById("evaluatedText");
+// let heading = document.getElementById("letterCount");
+// console.log(inputElement);
+// heading.innerText = 0;
+// inputElement.addEventListener("input", onChange);
+// function onChange() {
+// 	heading.innerText = inputElement.value.length;
+// }
+
+const enteredText = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+letterCount.textContent = 0;
+enteredText.addEventListener("input",() => {
+	let text = enteredText.value;
+	let count =text.length;
+
+	letterCount.textContent=count;
+});
